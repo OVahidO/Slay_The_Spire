4a448655fa36780f55357783e2cb53482aefb4d3
OVahidO <mr.vmn.ien@gmail.com>
2026-06-27 13:14:45 +0330

    feat: add Defend Card

--- skillcards.cpp
+++ skillcards.cpp
@@ -4,3 +4,14 @@ SkillCard::SkillCard(QString name, int energyCost, QString path, bool isRare, QG
     :Card(name, CardType::Skill, energyCost, path, isRare, parent) {}
 
 SkillCard::~SkillCard() {}
+
+Defend::Defend(QString path, QGraphicsItem *parent)
+    :SkillCard("Defend", 1, path, false, parent) {}
+
+void Defend::applyEffect(Player* player, Enemy* targetEnemy)
+{
+    Q_UNUSED(targetEnemy);
+
+    if(player != nullptr)
+        player->addBlock(5);
+}
--- skillcards.h
+++ skillcards.h
@@ -3,11 +3,19 @@
 
 #include "card.h"
 
-class SkillCard : Card
+class SkillCard : public Card
 {
 public:
     explicit SkillCard(QString name, int energyCost, QString path, bool isRare = false, QGraphicsItem *parent = nullptr);
     virtual ~SkillCard();
+};
+
+class Defend : public SkillCard
+{
+public:
+    explicit Defend(QString path, QGraphicsItem *parent = nullptr);
+
+    void applyEffect(Player* player = nullptr, Enemy* targetEnemy = nullptr);
 };
 
 #endif // SKILLCARDS_H
