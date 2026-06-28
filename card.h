216efaeb0b2d15a49fbdf8db1db55e3d367b90a3
OVahidO <mr.vmn.ien@gmail.com>
2026-06-28 17:22:41 +0330

    fix: Card base class=> change constructor & applyEffect method

--- card.h
+++ card.h
@@ -5,8 +5,8 @@
 #include <QObject>
 #include <QString>
 
-class Enemy;
-class Player;
+class Enemy {};
+class Player {};
 
 enum class CardType {Attack, Skill, Power, Status, Curse};
 
@@ -14,13 +14,13 @@ class Card : public QGraphicsObject
 {
     Q_OBJECT
 public:
-    explicit Card(QString name, CardType type, int energyCost, QString path, bool isRare = false, bool requiresTarget = true, QGraphicsItem *parent = nullptr, QString description = "Not described");
+    explicit Card(QString name, CardType type, int energyCost, QString path, QString description, bool isRare = false, bool requiresTarget = true, QGraphicsItem *parent = nullptr);
     virtual ~Card();
 
     virtual QRectF boundingRect() const override;
     virtual void paint(QPainter* painter, const QStyleOptionGraphicsItem *option, QWidget *widget) override;
 
-    virtual void applyEffect(Enemy* target, Player* player) = 0;
+    virtual void applyEffect(Player* player, Enemy* targetEnemy) = 0;
 
 signals:
 
@@ -30,9 +30,9 @@ protected:
     int m_energyCost;
     CardType m_type;
     QString m_sourcePath;
+    QString m_description;
     bool m_isRare;
     bool m_needTarget;
-    QString m_description;
 };
 
 #endif // CARD_H
