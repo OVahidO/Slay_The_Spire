73db086a7d2fcc25ac489f450f241dae1a8e552d
OVahidO <mr.vmn.ien@gmail.com>
2026-06-29 08:29:40 +0330

    feat: add Player attributes&methods

--- player.cpp
+++ player.cpp
@@ -13,3 +13,20 @@ QRectF Player::boundingRect() const
 
 void Player::paint(QPainter* painter, const QStyleOptionGraphicsItem *option, QWidget *widget)
 {}
+
+int Player::Hp() const {return m_Hp;}
+int Player::coin() const {return m_coin;}
+int Player::energy() const {return m_energy;}
+int Player::block() const {return m_block;}
+int Player::maxHp() const {return m_maxHp;}
+
+void Player::setHp(int hp) {m_Hp = (hp < 0)? 0 : hp ;}
+void Player::setCoin(int coin) {m_coin = (coin < 0)? 0 : coin;}
+void Player::setEnergy(int energy) {m_energy = (energy < 0)? 0 : energy;}
+void Player::setBlock(int block) {m_block = (block < 0)? 0 : block;}
+void Player::setMaxHp(int maxHp) {m_coin = (maxHp < 0)? 0 : maxHp;}
+
+void Player::addMaxHp(int n) {m_maxHp += (n < 0)? 0 : n;}
+void Player::addBlock(int n) {m_block += (n < 0)? 0 : n;}
+void Player::addEnergy(int n) {m_energy += (n < 0)? 0 : n;}
+void Player::loseHp(int n) {m_Hp -= (n < 0)? 0 : n; if(m_Hp < 0) {m_Hp = 0;} }
--- player.h
+++ player.h
@@ -15,7 +15,32 @@ public:
     virtual QRectF boundingRect() const override;
     virtual void paint(QPainter* painter, const QStyleOptionGraphicsItem *option, QWidget *widget) override;
 
+    int Hp() const;
+    int coin() const;
+    int energy() const;
+    int block() const;
+    int maxHp() const;
+
+    void setHp(int hp);
+    void setCoin(int coin);
+    void setEnergy(int energy);
+    void setBlock(int block);
+    void setMaxHp(int maxHp);
+
+    void addMaxHp(int n = 1);
+    void addBlock(int n = 1);
+    void addEnergy(int n = 1);
+    void loseHp(int n = 1);
+
+
 signals:
+
+private:
+    int m_Hp;
+    int m_coin;
+    int m_energy;
+    int m_block;
+    int m_maxHp;
 };
 
 #endif // PLAYER_H
