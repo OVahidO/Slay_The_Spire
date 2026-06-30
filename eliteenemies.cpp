ca7f9654a5f7b0970b06d1e04660790a896042f3
Ahoora Hematian <hematianlarkia@gmail.com>
2026-06-30 15:03:48 +0330

    created eliteenemies file

---
+++ eliteenemies.cpp
@@ -0,0 +1,3 @@
+#include "eliteenemies.h"
+
+GremlinKnob::GremlinKnob() {}
---
+++ eliteenemies.h
@@ -0,0 +1,12 @@
+#ifndef ELITEENEMIES_H
+#define ELITEENEMIES_H
+
+#include "enemy.h"
+
+class GremlinKnob : public Enemy
+{
+public:
+    GremlinKnob();
+};
+
+#endif // ELITEENEMIES_H
