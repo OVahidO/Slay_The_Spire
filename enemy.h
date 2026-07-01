266d7d1fa1d11ac82a6f6688d443f27ee57ce2c7
Ahoora Hematian <hematianlarkia@gmail.com>
2026-07-01 15:47:39 +0330

    nothing important

--- enemy.h
+++ enemy.h
@@ -2,45 +2,60 @@
 #define ENEMY_H
 
 #include <QGraphicsObject>
-#include <QObject>
-#include <QString>
 #include <QList>
+#include <QObject>
 #include <QPair>
+#include <QString>
 
 class Player;
 class BuffDebuff;
 class GamePlay;
 
-enum class IntentType { Attack, Defend, Buff, Debuff, AttackDebuff, DefendBuff, AttackDefend, Unknown };
+enum class IntentType {
+    Attack,
+    Defend,
+    Buff,
+    Debuff,
+    AttackDebuff,
+    DefendBuff,
+    AttackDefend,
+    Unknown
+};
 
-struct EnemyIntent {
+struct EnemyIntent
+{
     IntentType type = IntentType::Unknown;
     int value = 0;
     int secondaryValue = 0;
     bool isHidden = false;
 };
 
-class Enemy : public QGraphicsObject {
+class Enemy : public QGraphicsObject
+{
     Q_OBJECT
 public:
-    explicit Enemy(QString name, int minHP, int maxHP, bool isMultiplayer = false, QGraphicsItem *parent = nullptr);
+    explicit Enemy(QString name,
+                   int minHP,
+                   int maxHP,
+                   bool isMultiplayer = false,
+                   QGraphicsItem *parent = nullptr);
     virtual ~Enemy() = default;
 
     int takeDamage(int incomingDamage);
     void gainBlock(int amount);
     void resetBlock();
-    void executeIntent(Player* player);
+    void executeIntent(Player *player);
     bool isDead() const;
-    void applyEnemyIntent(GamePlay* game);
+    void applyEnemyIntent(GamePlay *game);
 
     QString getName() const;
     int getCurrentHP() const;
     int getMaxHP() const;
     EnemyIntent getCurrentIntent() const;
 
-    void addEffect(BuffDebuff* effect);
-    void removeEffect(BuffDebuff* effect);
-    QList<BuffDebuff*> getActiveEffects() const;
+    void addEffect(BuffDebuff *effect);
+    void removeEffect(BuffDebuff *effect);
+    QList<BuffDebuff *> getActiveEffects() const;
 
     virtual void calculateNextIntent() = 0;
 
@@ -48,7 +63,7 @@ public:
     // void paint(QPainter* painter, const QStyleOptionGraphicsItem *option, QWidget *widget) override;
 
 protected:
-    EnemyIntent pickIntent(const QList<QPair<int, EnemyIntent>>& options) const;
+    EnemyIntent pickIntent(const QList<QPair<int, EnemyIntent>> &options) const;
 
     QString m_name;
     int m_maxHP;
@@ -56,7 +71,7 @@ protected:
     int m_block;
     int m_turnCount;
     EnemyIntent m_currentIntent;
-    QList<BuffDebuff*> m_activeEffects;
+    QList<BuffDebuff *> m_activeEffects;
 };
 
 #endif // ENEMY_H
