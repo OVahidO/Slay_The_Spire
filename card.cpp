93d689e2ae586eea83283d3cf60fdbbe0fcb1211
Ahoora Hematian <hematianlarkia@gmail.com>
2026-07-02 16:33:45 +0330

    deleted path from constructor

--- card.cpp
+++ card.cpp
@@ -3,7 +3,6 @@
 Card::Card(QString name,
            CardType type,
            int energyCost,
-           QString path,
            QString description,
            bool isRare,
            bool isExhaust,
