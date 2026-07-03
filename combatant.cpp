9f790d50b1fb18b61195811359bd49dd0e1d58c3
Ahoora Hematian <hematianlarkia@gmail.com>
2026-07-03 16:20:01 +0330

    feat: added combatant class

---
+++ combatant.cpp
@@ -0,0 +1,78 @@
+#include "combatant.h"
+
+Combatant::Combatant(int maxHP, QGraphicsItem *parent)
+    : QGraphicsObject(parent)
+    , m_maxHP(maxHP)
+    , m_currentHP(maxHP)
+    , m_block(0)
+    , m_turnCount(0)
+{}
+
+int Combatant::takeDamage(int incomingDamage)
+{
+    int damageAfterBlock = incomingDamage - m_block;
+    if (damageAfterBlock < 0)
+        damageAfterBlock = 0;
+
+    m_block -= incomingDamage;
+    if (m_block < 0)
+        m_block = 0;
+
+    m_currentHP -= damageAfterBlock;
+    if (m_currentHP < 0)
+        m_currentHP = 0;
+
+    return damageAfterBlock;
+}
+
+void Combatant::addBlock(int amount)
+{
+    m_block += amount;
+}
+
+void Combatant::resetBlock()
+{
+    m_block = 0;
+}
+
+bool Combatant::isDead() const
+{
+    return m_currentHP <= 0;
+}
+
+int Combatant::currentHP() const
+{
+    return m_currentHP;
+}
+
+int Combatant::maxHP() const
+{
+    return m_maxHP;
+}
+int Combatant::block() const
+{
+    return m_block;
+}
+
+int Combatant::turnCount() const
+{
+    return m_turnCount;
+}
+void Combatant::nextTurn()
+{
+    m_turnCount++;
+}
+
+void Combatant::addEffect(BuffDebuff *effect)
+{
+    m_activeEffects.append(effect);
+}
+void Combatant::removeEffect(BuffDebuff *effect)
+{
+    m_activeEffects.removeOne(effect);
+}
+
+QList<BuffDebuff *> Combatant::getActiveEffects() const
+{
+    return m_activeEffects;
+}
---
+++ combatant.h
@@ -0,0 +1,40 @@
+#ifndef COMBATANT_H
+#define COMBATANT_H
+
+#include <QGraphicsObject>
+#include <QList>
+#include <QObject>
+
+class BuffDebuff;
+
+class Combatant : public QGraphicsObject
+{
+    Q_OBJECT
+public:
+    explicit Combatant(int maxHP, QGraphicsItem *parent = nullptr);
+    virtual ~Combatant() = default;
+
+    virtual int takeDamage(int incomingDamage);
+    void addBlock(int amount);
+    void resetBlock();
+    bool isDead() const;
+
+    int currentHP() const;
+    int maxHP() const;
+    int block() const;
+    int turnCount() const;
+    void nextTurn();
+
+    void addEffect(BuffDebuff *effect);
+    void removeEffect(BuffDebuff *effect);
+    QList<BuffDebuff *> getActiveEffects() const;
+
+protected:
+    int m_maxHP;
+    int m_currentHP;
+    int m_block;
+    int m_turnCount;
+    QList<BuffDebuff *> m_activeEffects;
+};
+
+#endif // COMBATANT_H
