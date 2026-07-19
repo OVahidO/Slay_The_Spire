77ef6ebd5f555308563a8cbeb1cc17cfe843563c
Ahoora Hematian <hematianlarkia@gmail.com>
2026-07-19 21:27:34 +0330

    set up Network lobby class

---
+++ networklobby.cpp
@@ -0,0 +1,5 @@
+#include "networklobby.h"
+
+NetworkLobby::NetworkLobby(QWidget *parent)
+    : QWidget{parent}
+{}
---
+++ networklobby.h
@@ -0,0 +1,37 @@
+#ifndef NETWORKLOBBY_H
+#define NETWORKLOBBY_H
+
+#include <QObject>
+#include <QWidget>
+
+class QLineEdit;
+class QPushButton;
+class QLabel;
+
+class NetworkLobby : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit NetworkLobby(QWidget *parent = nullptr);
+
+public slots:
+    void setStatusMessage(const QString &message);
+
+signals:
+    void hostRequested(quint16 port);
+    void joinRequested(const QString &address, quint16 port);
+
+private slots:
+    void onHostButtonClicked();
+    void onJoinButtonClicked();
+
+private:
+    QLineEdit *m_hostPortInput;
+    QLineEdit *m_joinAddressInput;
+    QLineEdit *m_joinPortInput;
+    QPushButton *m_hostButton;
+    QPushButton *m_joinButton;
+    QLabel *m_statusLabel;
+};
+
+#endif // NETWORKLOBBY_H
